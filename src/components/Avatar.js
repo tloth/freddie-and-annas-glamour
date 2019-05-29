dca92644941023a20940e3663513cf8bab882b62
Freddie <freddiepreece@outlook.com>
2019-05-29 16:26:20 +0100

    Add a state that stores time, decrement time each second, catch time if reaches zero

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Avatar = props => {
-    
+    return <div> </div>;
 }
 
 export default Avatar;
--- src/components/TimeBar.js
+++ src/components/TimeBar.js
@@ -2,6 +2,19 @@ import React from 'react';
 
 const TimeBar = props => {
 
+    const [time, setTime] = React.useState(10);
+
+    const decrement = () => {
+        setTime(oldTime => (oldTime === 0) ? 0 : oldTime - 1);
+        window.setTimeout(decrement, 1000);
+    }
+
+    return (
+    <div> 
+        <button onClick={decrement}>CLICK ME</button> 
+        <p>Time: {time}</p>
+    </div>
+         );
 }
 
 export default TimeBar;
