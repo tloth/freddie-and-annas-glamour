2df4fb794ecacb944554d444fcc88642d1e7e076
tloth <toth.annar@gmail.com>
2019-05-30 12:15:47 +0100

    hoist shared state up from Avatar to App, add basic info to avatar2

--- src/App.js
+++ src/App.js
@@ -6,11 +6,20 @@ import Avatar from './components/Avatar';
 import TimeBar from './components/TimeBar';
 
 function App() {
+  const [userData1, setUserData1] = React.useState(null);
+  const [userData2, setUserData2] = React.useState(null);
+
+
   return (
     <div className="App">
       <FlowerPile user='1'/>
       <TimeBar user='1'/>
-      <Avatar user='1'/>
+      <Avatar 
+        userData1={userData1}
+        setUserData1={setUserData1}
+        userData2={userData2}
+        setUserData2={setUserData2}
+      />
 
 
       <FlowerPile user='2'/>
