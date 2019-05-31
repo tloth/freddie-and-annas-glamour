d86e9770d390c61f012408deed91c35c5da5839f
Freddie <freddiepreece@outlook.com>
2019-05-31 11:58:18 +0100

    Add input field Relates #1

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import { getUserData } from '../utils/getUser';
 
 import { ReactComponent as AvatarLeft } from '../assets/avatar1.svg';
 import { ReactComponent as AvatarRight } from '../assets/avatar2.svg';
-import { ReactComponent as Flower1 } from '../assets/flowerHead1.svg';
-import { ReactComponent as Flower2 } from '../assets/flowerHead2.svg';
 
-const Avatar = ({ userData, setUserData, username, position }) => {
-
-    React.useEffect(() => {
-        getUserData(username).then(response => setUserData(response));
-    }, []);
+const Avatar = ({ userData, position }) => {
 
     if (!userData) {
         return <h3>...WAIT FOR IT</h3>;
     }
 
-    const { avatar_url, login, followers } = userData;
+    const { avatar_url, login } = userData;
 
     return (
         <div className="avatar">
