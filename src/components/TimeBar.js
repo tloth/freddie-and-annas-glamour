e5ed442feba4e6a1921cc5070bb54904da306ec6
tloth <toth.annar@gmail.com>
2020-01-20 13:24:32 +0000

    handle users with fewer than 10 followers

--- src/components/TimeBar.js
+++ src/components/TimeBar.js
@@ -3,31 +3,32 @@ import React from 'react';
 import { ReactComponent as InnerBar } from '../assets/innerBar.svg';
 
 const TimeBar = ({ userData, time, setTime }) => {
+  const [barHeight, setBarHeight] = React.useState(100);
 
-    const [barHeight, setBarHeight] = React.useState(100);
-
-    React.useEffect(() => {
-        if (userData && userData.followers > 10) {
-            setTime(userData.followers);
-        }
-    }, [userData, setTime]);
-
-    React.useEffect(() => {
-        if (userData) setBarHeight(time / userData.followers * 100);
-    }, [userData, time]);
-
-    const barStyle = {
-        height: barHeight + '%'
+  React.useEffect(() => {
+    if (userData && userData.followers >= 10) {
+      setTime(userData.followers);
+    } else if (userData && userData.followers < 10) {
+      setTime(10);
     }
+  }, [userData, setTime]);
 
-    return (
-        <div>
-            <div className='timeContainer'>
-                <div style={barStyle} className='innerBar'></div>
-            </div>
-            <p>TIME: {time}</p>
-        </div>
-    );
-}
+  React.useEffect(() => {
+    if (userData) setBarHeight((time / userData.followers) * 100);
+  }, [userData, time]);
+
+  const barStyle = {
+    height: barHeight + '%',
+  };
+
+  return (
+    <div>
+      <div className="timeContainer">
+        <div style={barStyle} className="innerBar"></div>
+      </div>
+      <p>TIME: {time}</p>
+    </div>
+  );
+};
 
 export default TimeBar;
